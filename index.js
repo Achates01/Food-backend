4b84303352a60c29d2bbf12aa91f4711e805048b
Sumit <sumitbui01@gmail.com>
2024-11-10 22:25:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// console.log("Food-Backend");
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
